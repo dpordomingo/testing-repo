9876a8dc719bddbb639a602a903a020837402ec9
David Pordomingo <david.pordomingo.f@gmail.com>
2018-09-12 18:15:43 +0200

    Create j.java
    Signed-off-by: David Pordomingo <david.pordomingo.f@gmail.com>

---
+++ j.java
@@ -0,0 +1,11 @@
+package com.iluwatar.callback;
+
+/**
+ * 
+ * Callback interface
+ * 
+ */
+public interface Callback {
+    // i hote java with a pasion tokuns gety weihgt lenhgt
+  void call();
+}
