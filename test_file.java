6d16be10a41fe1d4ce4523e91c7ef784b74be255
irinakhismatullina <40793894+irinakhismatullina@users.noreply.github.com>
2018-09-12 17:38:02 +0200

    Create test_file.java

---
+++ test_file.java
@@ -0,0 +1,12 @@
+package com.iluwatar.callback;
+
+/**
+ * 
+ * Callback interface
+ * 
+ */
+ // Callbac funktion 
+public interface Callback {
+    // i hote java with a pasion tokuns gety weihgt lenhgt
+  void call();
+}
