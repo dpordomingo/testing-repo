f0015c3ea484fb825b7079319c957ab3b5f4881c
David Pordomingo <David.Pordomingo.F@gmail.com>
2018-09-17 18:49:00 +0200

    Add
    Signed-off-by: David Pordomingo <David.Pordomingo.F@gmail.com>

---
+++ java.java
@@ -0,0 +1,11 @@
+// package descrition
+import java.io.*;
+
+/** 
+ * docummentation for java class
+ * multiline docummentation goes ere
+ */
+class HelloWorldApp {
+    /* multiline coment goes here
+    in newline */
+}
