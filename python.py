c436c52e9dd8540058054b36dffc6da5507fc8a6
David Pordomingo <david.pordomingo.f@gmail.com>
2018-09-17 15:58:24 +0200

    Update python.py
    Signed-off-by: David Pordomingo <david.pordomingo.f@gmail.com>

--- python.py
+++ python.py
@@ -6,3 +6,15 @@
 #
 
 a = Animal() # line comment
+
+class Foo(object):
+    """
+    Foo encapsulates a name
+    docummentation coment
+    """
+      
+    def __init__(self, name):
+        """
+        Deep docummentation
+        """
+        self.name = name
