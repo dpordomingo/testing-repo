50e57feab4eb05d9b64d32ab34b7370bc995cff6
David Pordomingo <David.Pordomingo.F@gmail.com>
2018-09-17 17:24:54 +0200

    Add example files
    Signed-off-by: David Pordomingo <David.Pordomingo.F@gmail.com>

---
+++ java.java
@@ -0,0 +1,11 @@
+// package descrition
+import java.io.*;
+
+/** 
+ * Docummentation for java class
+ * multiline docummentation goes ere
+ */
+class HelloWorldApp {
+    /* multiline coment goes ere
+    in newline */
+}
