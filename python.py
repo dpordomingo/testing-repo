71f96d45c4e2dc125a78c11cbea7d9772eed854b
David Pordomingo <David.Pordomingo.F@gmail.com>
2018-09-17 20:42:49 +0200

    Add
    Signed-off-by: David Pordomingo <David.Pordomingo.F@gmail.com>

---
+++ python.py
@@ -0,0 +1,20 @@
+# here is a coment
+
+#
+# block coment
+# thas a multiline
+#
+
+a = Animal() # line coment
+
+class Foo(object):
+    """
+    Foo encapsulates a nane
+    docummentation coment
+    """
+      
+    def __init__(self, name):
+        """
+        deep docummentation
+        """
+        self.name = name
